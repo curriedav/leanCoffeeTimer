2381b9bd91ddc7828df84c20086f47998db2e7f6
curriedav <curriedav@gmail.com>
2014-01-03 10:47:09 -0800

    refactor and bug fix

--- js/index.js
+++ js/index.js
@@ -1,72 +1,40 @@
 $(document).ready(function() {
-	
 	var timer = {
 		timerMin: {},
 		warnMin: {},
-		twoMinReset: function() {
-			clearInterval(this.timerMin);
-
-			var intTime = 120000;
+		timerReset: function(milles) {
+			clearInterval(timer.timerMin);
+			var intTime = milles;
 			var warnTime = intTime * 0.75;
 
 			$("#timerBox").removeClass("grey red orange").addClass("green");			
 			
 			warnAlarm = function() {
-				console.log("Only 1/4 time left!")
 				$("#timerBox").removeClass("green").addClass("orange");
-				clearInterval(this.warnMin);
+				clearInterval(timer.warnMin);
 			};
 
 			timerAlarm = function() {
-				console.log("Two minute timer has ended.");
 				$("#timerBox").removeClass("orange").addClass("red");
-				clearInterval(this.timerMin);
+				clearInterval(timer.timerMin);
 			};
 
-			console.log("Two minute timer is running...");
-			
-			this.timerMin = setInterval(timerAlarm, intTime);
-			this.warnMin = setInterval(warnAlarm, warnTime);
-		},
-		fiveMinReset: function() {
-			clearInterval(this.timerMin);
-
-			var intTime = 300000;
-			var warnTime = intTime * 0.75;
-
-			$("#timerBox").removeClass("grey red orange").addClass("green");			
-			
-			warnAlarm = function() {
-				console.log("Only 1/4 time left!")
-				$("#timerBox").removeClass("green").addClass("orange");
-				clearInterval(this.warnMin);
-			};
-
-			timerAlarm = function() {
-				console.log("Five minute timer has ended.");
-				$("#timerBox").removeClass("orange").addClass("red");
-				clearInterval(this.timerMin);
-			};
-
-			console.log("Five minute timer is running...");
-			
 			this.timerMin = setInterval(timerAlarm, intTime);
 			this.warnMin = setInterval(warnAlarm, warnTime);
 		},
 		stop: function() {
-			console.log("Timer stopped.");
 			$("#timerBox").removeClass("green red orange").addClass("grey");
-			clearInterval(this.timerMin);
-			clearInterval(this.warnMin);
+			clearInterval(timer.timerMin);
+			clearInterval(timer.warnMin);
 		}
 	};
 
 	$("#fiveMin").on('click', function() {
-		timer.fiveMinReset();
+		timer.timerReset(300000);
 	});
 
 	$("#twoMin").on('click', function() {
-		timer.twoMinReset();
+		timer.timerReset(120000);
 	});
 
 	$("#stop").on('click', function() {
